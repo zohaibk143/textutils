dcc58081027cf2c41d1b03228c648fbe51afa832
zohaibk143 <zohaibuii@gmail.com>
2022-02-17 01:00:44 +0500

    video 21

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,9 +25,7 @@ export default function TextForm(props) {
     }
     const handleCopy = ()=>{
         //console.log("i am copy.");
-        var text = document.getElementById('myBox');
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Copied to clipboard.", "success");
     }
     const handleExtraSpaces = () =>{
@@ -52,7 +50,7 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
             <h2>Your text summary.</h2>
-            <p>{text.split(" ").filter((element)=>{ return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{ return element.length!==0}).length} words and {text.length} characters</p>
             <p> {0.008 * text.split(" ").filter((element)=>{ return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:'Nothing to preview.'}</p>
